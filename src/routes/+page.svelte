b8f37663de35f20639b953511b71cfdb22b36782
LorisSigrist <loris@sigrist.dev>
2024-06-12 18:44:37 +0200

    Fx nojekyll

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,15 +5,15 @@
 <h1>Your name is <i>{m.your_name()}</i></h1>
 
 <style>
-    :global(html, body) {
-        position: fixed; 
-        inset: 0;
-        width: 100vw;
-        height: 100vh;
-    }
+  :global(html, body) {
+    position: fixed;
+    inset: 0;
+    width: 100vw;
+    height: 100vh;
+  }
 
-    :global(body) {
-        display: grid;
-        place-items: center;
-    }
+  :global(body) {
+    display: grid;
+    place-items: center;
+  }
 </style>
