ffed5958f9c095b0a43ae6785de640c3d8cdda38
LorisSigrist <loris@sigrist.dev>
2024-06-12 19:05:12 +0200

    update node version

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,1 @@
-// file initialized by the Paraglide-SvelteKit CLI - Feel free to edit it
-import { i18n } from "$lib/i18n"
-
-export const reroute = i18n.reroute()
+export const reroute = (url) => "/merge-berlin-lix-demo";
