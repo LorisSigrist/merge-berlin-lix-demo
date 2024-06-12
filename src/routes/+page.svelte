06b54229d31b16fd9e9d3fb9f018e1182333c499
LorisSigrist <loris@sigrist.dev>
2024-06-12 18:03:47 +0200

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import * as m from "$lib/paraglide/messages.js";
+</script>
+
+
+<h1>Your name is {m.your_name()}</h1>
+
+<style>
+    :global(html, body) {
+        position: fixed; 
+        inset: 0;
+    }
+
+    :global(body) {
+        display: grid;
+        place-items: center;
+    }
+</style>
