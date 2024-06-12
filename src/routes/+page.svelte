9602f00b2cd55fe03ff80a6e6868d688887f119a
LorisSigrist <loris@sigrist.dev>
2024-06-12 19:08:14 +0200

    update stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
   import * as m from "$lib/paraglide/messages.js";
-  import { i18n } from "$lib/i18n";
-  import { page } from "$app/stores";
 </script>
 
 <h1>Your name is <i>{m.your_name()}</i></h1>
