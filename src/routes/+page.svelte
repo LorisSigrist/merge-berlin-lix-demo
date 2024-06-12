de672defc1792bd533839142f79d399286a47d04
LorisSigrist <loris@sigrist.dev>
2024-06-12 18:27:41 +0200

    Add 😎style😎

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,14 @@
   import * as m from "$lib/paraglide/messages.js";
 </script>
 
-
-<h1>Your name is {m.your_name()}</h1>
+<h1>Your name is <i>{m.your_name()}</i></h1>
 
 <style>
     :global(html, body) {
         position: fixed; 
         inset: 0;
+        width: 100vw;
+        height: 100vh;
     }
 
     :global(body) {
